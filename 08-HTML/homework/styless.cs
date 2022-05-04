93d0e21259ce1c2b6432330135b133f59458c07d
NoeArregue <noearregue15@gmail.com>
2022-05-04 17:38:40 -0300

    first commit

---
+++ 08-HTML/homework/styless.cs
@@ -0,0 +1,18 @@
+h1 {
+    color: rgb(109, 12, 201);
+}
+img {
+    width: 400px;
+}
+#thirdDiv {
+    height: 600px;
+    width: 400px;
+    background-color:blueviolet;
+    padding: 50px;
+    border: 5px solid black;
+}
+#spanId
+{   
+    font-size: 18px;
+    margin: 50px;
+}
